198fe81a244a97e694038fc756821415aa2ce7d5
s990093 <128201642+s990093@users.noreply.github.com>
2023-03-18 23:44:26 +0800

    Update post_get.py

--- post_get.py
+++ post_get.py
@@ -7,5 +7,5 @@ print(response.json())
 
 # http://112.105.57.117:5000/
 obj = {"msg": "Hello, Server!"}
-r = requests.post("http://127.0.0.1:5000", data=obj)
+r = requests.post("http://127.0.0.1:5000", data=obj) #123
 print(r.json())
