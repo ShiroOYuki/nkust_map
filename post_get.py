c83814580e29918c3fb59882d6b241937207ae32
ShiroOYuki <63907529+ShiroOYuki@users.noreply.github.com>
2023-03-18 23:20:35 +0800

    Update post_get.py

--- post_get.py
+++ post_get.py
@@ -1,9 +1,11 @@
 import requests
 
+# http://112.105.57.117:5000/
 response = requests.get("http://127.0.0.1:5000")
 response.raise_for_status()
 print(response.json())
 
+# http://112.105.57.117:5000/
 obj = {"msg": "Hello, Server!"}
 r = requests.post("http://127.0.0.1:5000", data=obj)
 print(r.json())
